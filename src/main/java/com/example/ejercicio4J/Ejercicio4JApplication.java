6b58c41ab35646275f48a8bf68ebb4cc620e4fe0
rodrigo julca garcía <u00031348@sise.com.pe>
2024-11-28 20:01:56 -0500

    tarea

---
+++ src/main/java/com/example/ejercicio4J/Ejercicio4JApplication.java
@@ -0,0 +1,13 @@
+package com.example.ejercicio4J;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ejercicio4JApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ejercicio4JApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ejercicio4J/Ejercicio4JApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ejercicio4J;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ejercicio4JApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
